a0dc496b08f3def19d7a608c14038db277706d5e
Çağrı Dükünlü <cagridknl@gmail.com>
2023-02-17 10:07:27 +0300

    Implementasyon amaçlı basit bir boost kütüphanesi çalışması eklendi.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
 
-int main(int, char**) {
+using namespace boost::multiprecision;
+using namespace std;
+ 
+int128_t boost_product(long long A, long long B)
+{
+    int128_t ans = (int128_t)A * B;
+    return ans;
+}
+
+int main(int, char**) 
+{
+    long long first = 98745636214564698;
+    long long second = 7459874565236544789;
+    cout << "Product of " << first << " * " << second
+         << " = \n"
+         << boost_product(first, second)<<"\n";
     std::cout << "Hello, world!\n";
 }
+
